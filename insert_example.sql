0842b62090fcc7dec97eaf3c4cac7bd9bd6b536a
amehthamilton <33587080+amehthamilton@users.noreply.github.com>
2020-11-25 11:05:35 -0500

    Create insert_example.sql
    MS SQL - Example to insert records to a table.

---
+++ insert_example.sql
@@ -0,0 +1,12 @@
+--Statement to insert new record to the student table
+INSERT into student
+(studentId, lastname, firstname, addressline, city, state,zip)
+VALUES
+('846231','Smith','John','343 Miranda Ave','Miami','FL','33122'),
+('031289','James','Tim','454 Miranda Ave','Miami','FL','33234'),
+('033456820','Johnson','James','123 Miranda Ave','Miami','FL','33238')
+
+INSERT INTO student
+(studentId, lastname, firstname, addressline, city, state,zip)
+VALUES
+('033456820','Johnson','James','123 Miranda Ave','Miami','FL','33238')
